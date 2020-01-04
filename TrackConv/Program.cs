236be6ae6a7720aedf374bb480f1b62c484698b9
Miklós Pathy <miklos.pathy@pulispace.com>
2020-01-04 23:21:09 +0100

    XMRead added

--- TrackConv/Program.cs
+++ TrackConv/Program.cs
@@ -6,7 +6,39 @@ namespace TrackConv
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            XMRead xmreader = new XMRead(@"C:\Users\mpathy\Desktop\TrackerMuzax\modz\DAY 24 - 4mat-(Day24)-Blank_Page.xm");
+            xmreader.Open();
+
+            Console.WriteLine(xmreader.Header.IDString);
+            Console.WriteLine(xmreader.Header.Modulename);
+            Console.WriteLine(xmreader.Header.OneCharID);
+            Console.WriteLine(xmreader.Header.TrackerName);
+            Console.WriteLine("TrackerRevision: " + xmreader.Header.TrackerRevisionMajor + "." + xmreader.Header.TrackerRevisionMinor);
+            Console.WriteLine("HeaderSize: " + xmreader.Header.HeaderSize);
+            Console.WriteLine("SongLengthInPatterns: " + xmreader.Header.SongLengthInPatterns);
+            Console.WriteLine("RestartPosition: " + xmreader.Header.RestartPosition);
+            Console.WriteLine("NumberOfChannels: " + xmreader.Header.NumberOfChannels);
+            Console.WriteLine("NumberOfPatterns: " + xmreader.Header.NumberOfPatterns);
+            Console.WriteLine("NumberOfInstruments: " + xmreader.Header.NumberOfInstruments);
+            Console.WriteLine("Flags: " + xmreader.Header.Flags);
+            Console.WriteLine("DefaultTempo: " + xmreader.Header.DefaultTempo);
+            Console.WriteLine("DefaultBPM: " + xmreader.Header.DefaultBPM);
+            Console.WriteLine("PatternOrderTable: " + string.Join(' ', xmreader.Header.PatternOrderTable));
+
+            Console.WriteLine("Patterns:");
+            for (int i = 0; i < xmreader.Patterns.Length; i++)
+            {
+                XMRead.XMPattern pattern = xmreader.Patterns[i];
+                Console.WriteLine(i + ": " + pattern.LenghtOfPatternHeader + " " + pattern.PatternPackType + " " + pattern.NumberOfRows + " " + pattern.SizeOfPatternData);
+            }
+
+            Console.WriteLine("Instruments:");
+            for (int i = 0; i < xmreader.Instruments.Length; i++)
+            {
+                XMRead.XMInstrument instrument = xmreader.Instruments[i];
+                Console.WriteLine(i + ": " + instrument.NumberOfSamples + " " + instrument.InstrumentName + " " + instrument.Samples[0].NameOfSample + " " + instrument.nextinstrumentofset);
+            }
+
         }
     }
 }
