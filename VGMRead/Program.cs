310d1c8bf1e99ee4ecc043e14ebeec73b3b549c2
Miklós Pathy <miklos.pathy@pulispace.com>
2020-05-06 11:54:40 +0200

    Statistics class

--- VGMRead/Program.cs
+++ VGMRead/Program.cs
@@ -13,63 +13,8 @@ namespace VGMRead
             VGMRead reader = new VGMRead(@"C:\Users\mpathy\Desktop\TrackerMuzax\vgzs\06 Chani's Eyes.vgm");
             reader.Open();
 
+            Statistics.VGMWaitTotal(reader);
 
-            #region VGL Command statistics
-            Console.WriteLine("VGL Command statistics");
-            var commandstat = reader.VGMCommands.GroupBy(x => x.CommandByte).Select(group => new
-            {
-                Command = group.Key,
-                Count = group.Count()
-            }).OrderBy(x => x.Command).ToList();
-
-            foreach (var item in commandstat)
-            {
-                Console.WriteLine(item.Command.ToString("X") + " " + item.Count);
-            }
-            #endregion
-
-            #region OPL2 register usage statistics
-            Console.WriteLine("OPL2 register usage statistics");
-            var oplregwrites = reader.VGMCommands.Where(x => x is OPL2Command).Cast<OPL2Command>().ToList();
-            var e = oplregwrites.GroupBy(x => x.Register).Select(group => new
-            {
-                Register = group.Key,
-                Count = group.Count()
-            }).OrderBy(x => x.Register).ToList(); ;
-
-            foreach (var item in e)
-            {
-                Console.WriteLine(item.Register.ToString("X") + " " + item.Count);
-            }
-            #endregion
-
-            #region VGMWait usage statistics
-            Console.WriteLine("VGMWait usage statistics");
-            var waits = reader.VGMCommands.Where(x => x is VGMwait).Cast<VGMwait>().ToList();
-            var e2 = waits.GroupBy(x => x.waitsamples).Select(group => new
-            {
-                Waitsamples = group.Key,
-                Count = group.Count()
-            }).OrderBy(x => x.Waitsamples).ToList(); ;
-
-            foreach (var item in e2)
-            {
-                Console.WriteLine(item.Waitsamples + " " + VGMWait2mSec(item.Waitsamples).ToString("N3") + " " + item.Count);
-            }
-            #endregion
-
-            #region VGMWait total
-            Console.WriteLine("VGMWait total");
-            var waittotal = waits.Sum(x => x.waitsamples);
-            Console.WriteLine("Sum:" + waittotal);
-            Console.WriteLine("Header:" + reader.Header.Totalsamples);
-            Console.WriteLine("Sum in msec: " + VGMWait2mSec(waittotal).ToString("N3"));
-            #endregion
-        }
-
-        static double VGMWait2mSec(int waits)
-        {
-            return waits / 44.100;
         }
     }
 }
---
+++ VGMRead/Statistics.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VGMRead
+{
+    static class Statistics
+    {
+        public static void VGLCommandStatistics(VGMRead reader)
+        {
+            Console.WriteLine("VGL Command statistics");
+            var commandstat = reader.VGMCommands.GroupBy(x => x.CommandByte).Select(group => new
+            {
+                Command = group.Key,
+                Count = group.Count()
+            }).OrderBy(x => x.Command).ToList();
+
+            foreach (var item in commandstat)
+            {
+                Console.WriteLine(item.Command.ToString("X") + " " + item.Count);
+            }
+        }
+
+        public static void OPL2registerUsageStatistics(VGMRead reader)
+        {
+            Console.WriteLine("OPL2 register usage statistics");
+            var oplregwrites = reader.VGMCommands.Where(x => x is OPL2Command).Cast<OPL2Command>().ToList();
+            var e = oplregwrites.GroupBy(x => x.Register).Select(group => new
+            {
+                Register = group.Key,
+                Count = group.Count()
+            }).OrderBy(x => x.Register).ToList(); ;
+
+            foreach (var item in e)
+            {
+                Console.WriteLine(item.Register.ToString("X") + " " + item.Count);
+            }
+        }
+
+        public static void VGMWaitUsageStatistics(VGMRead reader)
+        {
+            Console.WriteLine("VGMWait usage statistics");
+            var waits = reader.VGMCommands.Where(x => x is VGMwait).Cast<VGMwait>().ToList();
+            var e2 = waits.GroupBy(x => x.waitsamples).Select(group => new
+            {
+                Waitsamples = group.Key,
+                Count = group.Count()
+            }).OrderBy(x => x.Waitsamples).ToList(); ;
+
+            foreach (var item in e2)
+            {
+                Console.WriteLine(item.Waitsamples + " " + VGMWait2mSec(item.Waitsamples).ToString("N3") + " " + item.Count);
+            }
+        }
+
+        public static void VGMWaitTotal(VGMRead reader)
+        {
+            Console.WriteLine("VGMWait total");
+            var waits = reader.VGMCommands.Where(x => x is VGMwait).Cast<VGMwait>().ToList();
+            var waittotal = waits.Sum(x => x.waitsamples);
+            Console.WriteLine("Sum:" + waittotal);
+            Console.WriteLine("Header:" + reader.Header.Totalsamples);
+            Console.WriteLine("Sum in msec: " + VGMWait2mSec(waittotal).ToString("N3"));
+        }
+
+        public static double VGMWait2mSec(int waits)
+        {
+            return waits / 44.100;
+        }
+    }
+}
